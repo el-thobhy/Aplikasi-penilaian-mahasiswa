2e733a81b8c142b65f060c1c1d8119092babcfe5
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-20 20:28:44 +0700

    Init project

---
+++ Soal BLQ/Program.cs
@@ -0,0 +1,42 @@
+﻿using Soal_BLQ;
+
+namespace Program
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("=== Assesment Soal BLQ ===");
+            new Program();
+        }
+
+        public Program()
+        {
+            string answer = "t";
+            Console.WriteLine("1. Soal 01");
+            Console.WriteLine("2. Soal 02");
+
+            while (answer.ToLower() == "t")
+            {
+
+                Console.Write("Masukkan no Soal: ");
+                int soal = int.Parse(Console.ReadLine());
+                switch (soal)
+                {
+                    case 1:
+                        new Soal01();
+                        break;
+                    case 2:
+                        new Soal02();
+                        break;
+                    default:
+                        Console.WriteLine("Soal tidak ditemukan");
+                        break;
+                }
+
+                Console.Write("Keluar? [y/t]: ");
+                answer = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Soal BLQ/Soal01.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Soal_BLQ
+{
+    internal class Soal01
+    {
+        public Soal01()
+        {
+            Console.WriteLine("Soal 01");
+        }
+    }
+}
