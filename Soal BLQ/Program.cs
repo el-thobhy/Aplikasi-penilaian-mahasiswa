9274aa3700fc6a0a770e0b853b2edd5fd1eb04b8
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-23 13:22:10 +0700

    Soal15

--- Soal BLQ/Program.cs
+++ Soal BLQ/Program.cs
@@ -25,6 +25,10 @@ namespace Program
             Console.WriteLine("10. Soal 10");
             Console.WriteLine("11. Soal 11");
             Console.WriteLine("12. Soal 12");
+            Console.WriteLine("13. Soal 13");
+            Console.WriteLine("14. Soal 14");
+            Console.WriteLine("15. Soal 15");
+
 
             while (answer.ToLower() == "t")
             {
@@ -69,6 +73,16 @@ namespace Program
                     case 12:
                         new Soal12();
                         break;
+                    case 13:
+                        new Soal13();
+                        break;
+                    case 14:
+                        new Soal14();
+                        break;
+                    case 15:
+                        new Soal15();
+                        break;
+
                     default:
                         Console.WriteLine("Soal tidak ditemukan");
                         break;
