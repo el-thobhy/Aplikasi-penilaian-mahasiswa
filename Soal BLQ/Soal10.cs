7f07657a37dc37c749c886c3667e5d04c6eb0418
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-26 09:06:01 +0700

    Soal 10

--- Soal BLQ/Soal10.cs
+++ Soal BLQ/Soal10.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Soal_BLQ
 {
@@ -10,7 +11,31 @@ namespace Soal_BLQ
     {
         public Soal10()
         {
-            
+            Console.WriteLine("=== Soal 10 ===");
+            Console.Write("Masukkan Nama : ");
+            string input = Console.ReadLine();
+            string output;
+
+            string[] kata = input.Split(' ');
+
+            for (int i = 0; i < kata.Length; i++)
+            {
+                if (kata[i].Length > 1)
+                {
+                    char[] chars = kata[i].ToCharArray();
+                    for (int j = 1; j < chars.Length - 1; j++)
+                    {
+                        chars[j] = '*';
+                    }
+                    kata[i] = new string(chars);
+                }
+            }
+
+            output = string.Join(" ", kata);
+
+            Console.WriteLine("Output: " + output);
+
         }
+
     }
 }
