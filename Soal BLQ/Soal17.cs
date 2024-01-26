e7de154f3caf6db5d489109f37560a13675f582d
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-26 10:33:48 +0700

    Soal 17

--- Soal BLQ/Soal17.cs
+++ Soal BLQ/Soal17.cs
@@ -10,7 +10,27 @@ namespace Soal_BLQ
     {
         public Soal17()
         {
-            
+            Console.WriteLine("=== SOal 17 ===");
+            Console.WriteLine("Contoh Input : N N T N N N T T T T T N T T T N T N");
+            Console.Write("Masukkan Perjalanan Hattori : ");
+            string input = Console.ReadLine().Replace(" ", "");
+
+            int gunung = 0;
+            int lembah = 0;
+            int ketinggian = 0;
+
+            foreach (char langkah in input)
+            {
+                ketinggian += (langkah == 'N') ? 1 : -1;
+
+                if (ketinggian == 0 && langkah == 'N')
+                    lembah++;
+                else if (ketinggian == 0 && langkah == 'T')
+                    gunung++;
+            }
+
+            Console.WriteLine($"Jumlah Gunung: {gunung}");
+            Console.WriteLine($"Jumlah Lembah: {lembah}");
         }
     }
 }
