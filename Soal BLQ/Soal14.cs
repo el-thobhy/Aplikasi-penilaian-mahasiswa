b0d1f9d339070ca3920cd057df73fb414b5801bc
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-26 10:16:35 +0700

    Soal 14

--- Soal BLQ/Soal14.cs
+++ Soal BLQ/Soal14.cs
@@ -10,7 +10,33 @@ namespace Soal_BLQ
     {
         public Soal14()
         {
-            
+            Console.WriteLine("=== Soal 14: Rotasi deret");
+            Console.Write("Masukkan deret (contoh: 5 6 7 0 1): ");
+            List<string> list = Console.ReadLine().Split(' ').ToList();
+            List<int> nums = list.ConvertAll(int.Parse);
+
+            Console.Write("Masukkan Rotasi: ");
+            int rotasi = int.Parse(Console.ReadLine());
+
+
+            Printing.PrintList(RotationDeret(nums, rotasi));
+        }
+
+        private List<int> RotationDeret(List<int> nums, int rotasi)
+        {
+            int temp = nums[0];
+            for (int j = 0; j < nums.Count - 1; j++)
+            {
+                nums[j] = nums[j + 1];
+            }
+            rotasi -= 1;
+            nums[nums.Count - 1] = temp;
+            //Printing.PrintList(nums);
+            if (rotasi > 0)
+            {
+                RotationDeret(nums, rotasi);
+            }
+            return nums;
         }
     }
 }
