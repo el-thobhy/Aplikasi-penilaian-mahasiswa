169f7b9b0ad1c8a0b29f5ce5a4cf800c3b46e643
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-26 10:10:43 +0700

    Soal 13

--- Soal BLQ/Soal13.cs
+++ Soal BLQ/Soal13.cs
@@ -10,7 +10,23 @@ namespace Soal_BLQ
     {
         public Soal13()
         {
-            
+            Console.WriteLine("=== Soal 13 ===");
+            Console.Write("Masukkan waktu (format: HH:mm): ");
+            string input = Console.ReadLine();
+
+            string[] splitInput = input.Split(':');
+            double jam = double.Parse(splitInput[0]);
+            double menit = double.Parse(splitInput[1]);
+            double posisiMenit = (menit / 60) * 12;
+            double sudut = 360/12;
+
+            jam += (menit / 60);
+            jam = (jam >= 12) ? jam - 12 : jam;
+
+            double output = Math.Abs(jam - posisiMenit) * sudut;
+            output = (output > 180) ? 360 - output : output;
+
+            Console.WriteLine($"{Math.Round(output)}");
         }
     }
 }
