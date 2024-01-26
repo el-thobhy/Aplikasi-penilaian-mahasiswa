0bda41fc7878dcde4adc22ac57a5394cef92e2c0
el-thobhy <auriwan.yasper@sci.ui.ac.id>
2024-01-26 10:46:44 +0700

    tes

--- Soal BLQ/Soal17.cs
+++ Soal BLQ/Soal17.cs
@@ -10,10 +10,33 @@ namespace Soal_BLQ
     {
         public Soal17()
         {
-            Console.WriteLine("=== SOal 17 ===");
+            Console.WriteLine("=== Soal 17 ===");
             Console.WriteLine("Contoh Input : N N T N N N T T T T T N T T T N T N");
             Console.Write("Masukkan Perjalanan Hattori : ");
             string input = Console.ReadLine().Replace(" ", "");
+            //gunung
+
+            //Menanjak (N) dari ketinggian 0 menjadi 1.
+            //Menanjak (N) dari ketinggian 1 menjadi 2.
+            //Turun (T) dari ketinggian 2 menjadi 1.
+            //Menanjak (N) dari ketinggian 1 menjadi 2.
+            //Menanjak (N) dari ketinggian 2 menjadi 3.
+            //Menanjak (N) dari ketinggian 3 menjadi 4.
+            //Turun (T) dari ketinggian 4 menjadi 3.
+            //Turun (T) dari ketinggian 3 menjadi 2.
+            //Turun (T) dari ketinggian 2 menjadi 1.
+            //Turun (T) dari ketinggian 1 menjadi 0.
+
+            //lembah
+
+            //Menanjak(N) dari ketinggian 0 menjadi 1.
+            //Turun(T) dari ketinggian 1 menjadi 0.
+            //Turun(T) dari ketinggian 0 menjadi - 1.
+            //Turun(T) dari ketinggian -1 menjadi - 2.
+            //Turun(T) dari ketinggian -2 menjadi - 3.
+            //Menanjak(N) dari ketinggian -3 menjadi - 2.
+            //Turun(T) dari ketinggian -2 menjadi - 3.
+            //Menanjak(N) dari ketinggian -3 menjadi - 2.
 
             int gunung = 0;
             int lembah = 0;
@@ -25,7 +48,7 @@ namespace Soal_BLQ
 
                 if (ketinggian == 0 && langkah == 'N')
                     lembah++;
-                else if (ketinggian == 0 && langkah == 'T')
+                else if (langkah == 'T')
                     gunung++;
             }
 
